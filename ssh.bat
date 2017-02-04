f8cbec9940d247ad5bd0757912cbf99c4c4a5ef9
Leigh McCulloch <leigh@mcchouse.com>
2017-02-05 08:52:24 +1100

    Add support for all windows drive letters when mapping to /mnt/x
    Fixes #1

--- ssh.bat
+++ ssh.bat
@@ -2,7 +2,34 @@
 setlocal enabledelayedexpansion
 
 set args=%*
-if not "%1"=="" set args=!args:C:=/mnt/c!
+if not "%1"=="" (
+	set args=!args:A:=/mnt/a!
+	set args=!args:B:=/mnt/b!
+	set args=!args:C:=/mnt/c!
+	set args=!args:D:=/mnt/d!
+	set args=!args:E:=/mnt/e!
+	set args=!args:F:=/mnt/f!
+	set args=!args:G:=/mnt/g!
+	set args=!args:H:=/mnt/h!
+	set args=!args:I:=/mnt/i!
+	set args=!args:J:=/mnt/j!
+	set args=!args:K:=/mnt/k!
+	set args=!args:L:=/mnt/l!
+	set args=!args:M:=/mnt/m!
+	set args=!args:N:=/mnt/n!
+	set args=!args:O:=/mnt/o!
+	set args=!args:P:=/mnt/p!
+	set args=!args:Q:=/mnt/q!
+	set args=!args:R:=/mnt/r!
+	set args=!args:S:=/mnt/s!
+	set args=!args:T:=/mnt/t!
+	set args=!args:U:=/mnt/u!
+	set args=!args:V:=/mnt/v!
+	set args=!args:W:=/mnt/w!
+	set args=!args:X:=/mnt/x!
+	set args=!args:Y:=/mnt/y!
+	set args=!args:Z:=/mnt/z!
+)
 
 set bashexe=bash.exe
 if %PROCESSOR_ARCHITECTURE%==x86 (
